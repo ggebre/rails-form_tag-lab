9bc95e7b5e5b519c36b7a263371e1546c9e9daf6
Getu Gebre <getu.gebre.gg@gmail.com>
2020-11-26 18:15:24 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student(first_name: "Getu", last_name: "Gebre")
+Student(first_name: "Samuel", last_name: "Gebre")
